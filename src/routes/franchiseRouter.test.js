c735eb838d438a08dcf967eb63e4189ff0166edf
ZonkeyZebra <blackcec@yahoo.com>
2026-01-27 18:54:00 -0700

    improved test start

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,30 +1,32 @@
-process.env.NODE_ENV = 'test';
-process.env.DB_NAME = 'myapp_test';
-// or
-process.env.DATABASE_URL = 'mysql://localhost/myapp_test';
-
 const request = require('supertest');
 const app = require('../service');
-const db = require('../database/database');
 
-const testAdmin = { name: 'pizza franchisee', email: 'franchise@test.com', password: 'a', roles: [{ role: 'admin' }] };
 // const regularUser = { name: 'pizza please', email: 'email@test.com', password: 'b' };
 
 let adminAuthToken;
 
+const { Role, DB } = require('../database/database.js');
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@adminTest.com';
+
+    user = await DB.addUser(user);
+    return { ...user, password: 'toomanysecrets' };
+}
+
 beforeAll(async () => {
     // make email unique per test run
-    testAdmin.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    try {
-        await db.DB.addUser(testAdmin);
-    } catch (err) {
-        console.log(err);
-    }
+    const testAdmin = createAdminUser();
+    const admin = await testAdmin
 
     // login admin user
-    // const registerRes = await request(app).post('/api/auth').send(testAdmin);
-    const loginRes = await request(app).put('/api/auth').send(testAdmin);
-
+    const loginRes = await request(app).put('/api/auth').send(admin);
     adminAuthToken = loginRes.body.token;
 
     // create a franchise to use in tests
@@ -32,8 +34,8 @@ beforeAll(async () => {
         .post('/api/franchise')
         .set('Authorization', `Bearer ${adminAuthToken}`)
         .send({
-            name: 'pizzaPocket',
-            admins: [{ email: testAdmin.email }],
+            name: randomName(),
+            admins: [{ email: admin.email }],
         });
     // expect(franchiseRes.status).toBe(200);
     console.log(franchiseRes.body);

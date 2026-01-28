08e8db00a6da1eef1a8b5123361f776d89b08e8d
ZonkeyZebra <blackcec@yahoo.com>
2026-01-27 20:34:24 -0700

    create store test

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -56,3 +56,17 @@ test('delete franchise', async () => {
     const deleteFranchiseRes = await request(app).delete(`/api/franchise/${franchiseId}`).set('Authorization', `Bearer ${adminAuthToken}`);
     expect(deleteFranchiseRes.status).toBe(200);
 });
+
+test('create franchise store', async () => {
+    const franchiseRes = await request(app)
+        .post('/api/franchise')
+        .set('Authorization', `Bearer ${adminAuthToken}`)
+        .send({
+            name: randomName(),
+            admins: [{ email: admin.email }],
+        });
+    franchiseId = franchiseRes.body.id;
+    const store = { name: randomName() };
+    const createStore = await request(app).post(`/api/franchise/${franchiseId}/store`).set('Authorization', `Bearer ${adminAuthToken}`).send(store);
+    expect(createStore.status).toBe(200);
+});

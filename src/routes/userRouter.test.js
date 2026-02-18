59ac03d07278101c76e5ff1df6244ffa4dd9653a
ZonkeyZebra <blackcec@yahoo.com>
2026-02-17 19:49:15 -0700

    added list user tests

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -49,7 +49,28 @@ test('update a user', async () => {
     expect(updatedUser.status).toBe(200);
 });
 
-test('list users', async () => {
+test('list users unauthorized', async () => {
     const listUsersRes = await request(app).get('/api/user');
+    expect(listUsersRes.status).toBe(401);
+});
+
+test('list users', async () => {
+    const [user, userToken] = await registerUser(request(app));
+    expect(user).toBeDefined();
+    const listUsersRes = await request(app)
+        .get('/api/user')
+        .set('Authorization', 'Bearer ' + userToken);
     expect(listUsersRes.status).toBe(200);
 });
+
+async function registerUser(service) {
+    const testUser = {
+        name: 'pizza diner',
+        email: `${randomName()}@test.com`,
+        password: 'a',
+    };
+    const registerRes = await service.post('/api/auth').send(testUser);
+    registerRes.body.user.password = testUser.password;
+
+    return [registerRes.body.user, registerRes.body.token];
+}
